087ddba7461fd0ef7a273f6769895eba4481e795
AnaBit <anabit@hotmail.com>
2016-10-14 18:55:37 +0800

    init

---
+++ DevIO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DevIO
+//
+//  Created by Atropos on 2016/10/14.
+//  Copyright © 2016年 AnaBit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
